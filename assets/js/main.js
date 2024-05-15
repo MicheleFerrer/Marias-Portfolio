b307288fc7bcc31361921d3643ff0e4b5987efcc
Michele Ferrer <michele.e.ferrer@gmail.com>
2024-05-15 19:57:26 -0400

    merging branches (#4)
    * Home button not translating
    
    * realized the zindex was incorrect therefore causing the transform property to not work on home__button
    
    * adding swiper JS
    
    * swiper js installed everything working well :)
    
    * Parallax added successfully

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,31 @@ const linkAction = () =>{
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*initializing swiper js*/ 
+
+const swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    speed: 800,
+    parallax: true,
+    effect: 'fade',
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+
+      formatFractionCurrent: (number) => { return '0' + number },
+      formatFractionTotal: (number) => { return '0' + number }
+
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+=======
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
